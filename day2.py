7bb0ecc2cf0f8a16738ee8429c63f70d0f9cde04
Your Name <you@example.com>
2023-12-02 12:37:54 +0100

    Tried a more map-reduce-ish approach

--- day2.py
+++ day2.py
@@ -1,5 +1,5 @@
 import sys
-import functools
+from functools import reduce
 import operator
 
 bag = {"red": 12, "green": 13, "blue": 14}
@@ -26,6 +26,7 @@ def split_turns(game: str) -> list[str]:
 
 
 def accumulate_game(line: str) -> (int, int):
+    line = line.strip()
     accumulator = {}
     game_id, game = line.split(": ")
     game_id = int(game_id.split(" ")[-1])
@@ -33,11 +34,12 @@ def accumulate_game(line: str) -> (int, int):
     for turn in turns:
         draws = parse_draws(turn)
         accumulate_draws(accumulator, draws)
-    power = functools.reduce(operator.mul, accumulator.values(), 1)
+    power = reduce(operator.mul, accumulator.values(), 1)
     return game_id, power
 
 
 def validate_game(line: str) -> (int, bool):
+    line = line.strip()
     game_id, game = line.split(": ")
     game_id = int(game_id.split(" ")[-1])
     turns = split_turns(game)
@@ -49,14 +51,19 @@ def validate_game(line: str) -> (int, bool):
     return game_id, True
 
 
-
 def part_1(entries: list[str]):
     total = 0
     for line in entries:
         game_id, ok = validate_game(line.strip())
         if ok:
             total += game_id
+    print("Part 1:", total)
 
+
+def part_1_mr(entries: list[str]):
+    validations = map(validate_game, entries)
+    okay = [game_id for (game_id, ok) in validations if ok]
+    total = sum(okay)
     print("Part 1:", total)
 
 
@@ -69,8 +76,15 @@ def part_2(entries: list[str]):
     print("Part 2:", total)
 
 
+def part_2_mr(entries: list[str]):
+    accumulations = map(accumulate_game, entries)
+    powers = [power for (game_id, power) in accumulations]
+    total = sum(powers)
+    print("Part 2:", total)
+
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         lines = f.readlines()
-        part_1(lines)
-        part_2(lines)
+        part_1_mr(lines)
+        part_2_mr(lines)

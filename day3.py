79744dc4c1459586e64c59585ac1bf08957bda29
Your Name <you@example.com>
2023-12-03 14:13:33 +0100

    Alternate solution for Part 2

--- day3.py
+++ day3.py
@@ -16,7 +16,7 @@ def symbols_in_range(array: list[str], span: (int, int), row: int) -> set[str]:
 
 def collect_adjacent_symbols(array: list[str], span: (int, int), row: int) -> set[str]:
     symbols = set()
-    for r in range(row-1, row+2):
+    for r in range(row - 1, row + 2):
         symbols |= symbols_in_range(array, span, r)
     return symbols
 
@@ -42,7 +42,7 @@ def stars_in_range(array: list[str], span: (int, int), row: int) -> set[(int, in
 
 def collect_adjacent_stars(array: list[str], span: (int, int), row: int) -> set[(int, int)]:
     stars = set()
-    for r in range(row-1, row+2):
+    for r in range(row - 1, row + 2):
         stars |= stars_in_range(array, span, r)
     return stars
 
@@ -72,15 +72,42 @@ def find_numbers_with_stars(array: list[str]) -> list[(int, list[(int, int)])]:
     return numbers_and_stars
 
 
+def find_all_stars(array: list[str]) -> list[(int, int, int)]:
+    numbers = []
+    for index, line in enumerate(array):
+        for match in re.finditer(r"\*", line):
+            span = match.span()
+            numbers.append((span[0], index))
+    return numbers
+
+
+def find_all_numbers(array: list[str]) -> list[(int, (int, int, int))]:
+    numbers = []
+    for row, line in enumerate(array):
+        for match in re.finditer(r"\d+", line):
+            span = match.span()
+            numbers.append((int(match.group()), (span[0], span[1], row)))
+    return numbers
+
+
 def stars_for_numbers(data: list[(int, list[(int, int)])]) -> dict[(int, int), list[int]]:
     lookup: dict[(int, int), list[int]] = {}
     for item in data:
         number, coordinates = item
         for coord in coordinates:
-            if coord in lookup:
-                lookup[coord].append(number)
-            else:
-                lookup[coord] = [number]
+            lookup.setdefault(coord, []).append(number)
+    return lookup
+
+
+def stars_for_numbers_alt(numbers: list[(int, (int, int, int))], stars: list[(int, int)]) -> dict[(int, int), list[int]]:
+    lookup: dict[(int, int), list[int]] = {}
+    for number, coordinates in numbers:
+        start, end, row = coordinates
+        for position in stars:
+            column, line = position
+            if (line-1 <= row <= line+1) and (start-1 <= column < end+1):  # this part is disgusting
+                lookup.setdefault(position, []).append(number)
+
     return lookup
 
 
@@ -96,8 +123,18 @@ def part_2(lines: list[str]):
     print("Part 2:", sum(ratios))
 
 
+def part_2_alt(lines: list[str]):
+    numbers = find_all_numbers(lines)
+    stars = find_all_stars(lines)
+    lookup = stars_for_numbers_alt(numbers, stars).items()
+    gears = [(coord, numbers) for coord, numbers in lookup if len(numbers) == 2]
+    ratios = [g[0] * g[1] for _, g in gears]
+    print("Part 2 alt:", sum(ratios))
+
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         text = [line.strip() for line in f.readlines()]
         part_1(text)
         part_2(text)
+        part_2_alt(text)
